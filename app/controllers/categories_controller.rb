459c992c89541f61b6549e141238820228bb63a9
Jacob Golding <jcbgldng@gmail.com>
2016-08-26 10:59:54 -0500

    create the path to add new categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,14 @@ class CategoriesController < ApplicationController
 	def new
 		@category = Category.new
 	end
+
+	def create
+		@category = Category.new(params[:category].permit(:title))
+		if @category.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,24 +4,26 @@ Rails.application.routes.draw do
 
   post 'users', to: 'users#create'
 
-  get '/users/new', to:  'users#new', as: 'new_user'
+  get '/users/new', to:  'users#new', as: 'users_new'
 
   get 'users/:id', to: 'users#show', as: 'user'
 
 
   post '/articles', to: 'articles#create'
   
-  get '/articles/new', to: 'articles#new', as: 'new_article'
+  get '/articles/new', to: 'articles#new', as: 'articles_new'
 
   get '/articles/:id', to:  'articles#show', as: 'article'
 
 
   post '/login' => 'sessions#create'
 
-  get '/login', to: 'sessions#new', as: 'new_session'
+  get '/login', to: 'sessions#new', as: 'sessions_new'
 
   delete '/login', to: 'sessions#destroy'
 
+
+  post 'categories', to: 'categories#create'
 
   get 'categories/new', to: 'categories#new', as: 'new_category'
 end
