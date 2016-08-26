b580be302feeb42f78999729288fb43b7d58367e
Ben Van Sickle <email": null>
2016-08-25 19:53:12 -0500

    create article working. HTML now rendering from strings

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,15 +18,32 @@ class ArticlesController < ApplicationController
   def create
     params.inspect
     if current_user && current_user.role.user?
-      @article = Article.new(params[:title])
+      @article = Article.new(article_params)
       if @article.save
-        @revision = Revision.new(article_id: @article, body: params[:body], editor_id: current_user)
+        @revision = Revision.new(article_id: @article.id, body: revision_params[:body], editor_id: current_user.id)
         if @revision.save
-          params.each do ||
-
+          params.each do |param|
+            p "*****************"
+            p param
+            if param.to_i != 0
+              @article.categories << Category.find(param.to_i)
+            end
+          end
         end
       else
       end
     end
+    redirect_to root_path
   end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title)
+  end
+
+  def revision_params
+    params.require(:revision).permit(:body)
+  end
+
 end
